70751e192bd014c7b7a757658a7846c3db08601a
garretblankenship <garret.blankenship@coderacademy.edu.au>
2020-01-09 12:20:45 +1100

    class

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import "./../styles/App.css";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import BookmarksPage from "./pages/BookmarksPage";
 
 class App extends Component {
-    state = { token: null };
+    state = { token: sessionStorage.getItem("token") };
 
     onRegisterFormSubmit = (token, callback = () => {}) => {
+        sessionStorage.setItem("token", token);
         this.setState({ token }, callback);
     }
     
@@ -25,6 +27,7 @@ class App extends Component {
                                 return <RegisterPage {...props} onRegisterFormSubmit={this.onRegisterFormSubmit} />
                             }}  
                         />
+                        <Route exact path="/bookmarks" component={BookmarksPage} />
                         <Route component={NotFoundPage} />
                     </Switch>
                 </div>
---
+++ src/components/pages/BookmarksPage.js
@@ -0,0 +1,3 @@
+//create class component BookmarksPage
+//keeps track of a single piece of state which is booksmarks
+//defaults to empty array
