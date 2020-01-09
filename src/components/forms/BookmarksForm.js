c56c3c58103a65074fb584d273ec16a38282d5d1
garretblankenship <garret.blankenship@coderacademy.edu.au>
2020-01-09 12:56:19 +1100

    class

---
+++ src/components/forms/BookmarksForm.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+
+class BookmarksForm extends Component {
+    state = {
+        title: "",
+        url: ""
+    }
+
+    onInputChange = (event) => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        const { title, url } = this.state;
+
+        return (
+            <form onSubmit={this.onFormSubmit} >
+                <div>
+                    <label>Title</label>
+                    <input name="title" value={title} onChange={this.onInputChange} />
+                </div>
+                <div>
+                    <label>Url</label>
+                    <input name="url" value={url} onChange={this.onInputChange} />
+                </div>
+                <input type="submit" value="Create New Bookmark" />
+            </form>
+        )
+    }
+}
+
+export default BookmarksForm;
